b319b9c1883bc88e8a949d95f04073aa440571bb
kasinee0806 <58645576+kasinee0806@users.noreply.github.com>
2019-12-08 11:29:01 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World,I am Nan");
-        System.out.println("Kasinee");
+        System.out.println("Kasinee Boontawe");
+        System.out.println("361211760023");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
         System.out.println("saiyai");
