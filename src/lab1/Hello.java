ed10a34050b2130ccba847a0aeda489ff4414d51
kasinee <kasinee9803@gmail.com>
2019-12-08 11:11:23 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello World,I am Nan");
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
 
 
 
